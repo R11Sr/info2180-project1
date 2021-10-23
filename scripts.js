c96f8b834ed76e0ec8e04514486cbec89b42ea24
r11Sr <rs_burner2@outlook.com>
2021-10-23 00:42:55 -0500

    added JS folm validation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+    let form = document.querySelector("form");
+    let button = document.querySelector("form button");
+
+    
+    button.addEventListener("click", e =>{
+        let email = document.getElementById("email").value;
+        // console.log( document.getElementsByClassName("message"));
+
+        let notification = document.getElementsByClassName("message")[0];
+        if (email === "") {
+            
+            notification.innerText = "Please enter your email address";     
+        }
+        else{
+            notification.innerText = `Thank you! Your email address ${email}`;     
+
+        }
+    });
+
+
+
+
+});
